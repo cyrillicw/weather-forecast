1716dcde09b641dd5188c4347df3dc4d4ea33b07
cyrillicw <cyrillicw@gmail.com>
2019-04-13 16:58:40 +0300

    dynamic info

---
+++ app/src/main/java/com/example/android/myapplication/pojo/Clouds.java
@@ -0,0 +1,12 @@
+package com.example.android.myapplication.pojo;
+
+public class Clouds {
+    private int all;
+
+    public void setAll(int all){
+        this.all = all;
+    }
+    public int getAll(){
+        return this.all;
+    }
+}
